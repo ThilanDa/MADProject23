1d08a20ada5084cc468cb8a84117c0d7259bf011
AkmeemanaTD <99232376+AkmeemanaTD@users.noreply.github.com>
2023-05-04 20:21:41 +0530

    commit1

--- app/src/main/java/com/example/madproject/MainActivity.kt
+++ app/src/main/java/com/example/madproject/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.madproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firebase : DatabaseReference = FirebaseDatabase.getInstance().getReference()
+
     }
 }
